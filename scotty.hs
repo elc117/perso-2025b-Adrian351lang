68c96ecd711e2e044db0c1a370dc7499b952ad1a
Adrian351lang <177791808+Adrian351lang@users.noreply.github.com>
2025-09-19 18:07:42 -0300

    teste /bases e /morse

--- scotty.hs
+++ scotty.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 
@@ -6,5 +7,8 @@ main :: IO ()
 main = scotty 3000 $ do
   middleware logStdoutDev
 
-  get "/" $ do
-    text "hello world"
+  get "/bases" $ do
+    text "hello bases"
+
+  get "/morse" $ do
+    text "hello morse"
